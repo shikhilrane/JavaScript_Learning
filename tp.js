0ac84528359e654e7e4ead58981deba4a0b2f1d5
Shikhil Rane <shikhilrane04@gmail.com>
2026-03-07 21:16:25 +0530

    Replaces array processing with nested function logic
    Introduces a function that demonstrates closure and
    higher-order function concepts, replacing prior array
    spread usage. Enhances code clarity and functional abstraction.

--- tp.js
+++ tp.js
@@ -1,6 +1,10 @@
-let names = ["Pune", "Mumbai", "Nashik", "Nagpur"];
-function getNames(...arr) {
-    console.log(arr);
+function add(a,b) {
+    console.log(a+b);
+    return function newFunc(c,d) {
+        console.log(c * d);
+    };
 }
 
-getNames(...names);
+let outer = add(3,4);
+let inner = outer(5,6);
+console.log(inner);
